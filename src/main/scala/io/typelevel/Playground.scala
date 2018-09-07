dc721126b11296c1608e05c7118ba6381a9884c4
Jakub Kozłowski <kubukoz@gmail.com>
2018-09-07 02:54:49 +0200

    Initial commit

---
+++ src/main/scala/io/typelevel/Playground.scala
@@ -0,0 +1,9 @@
+package io.typelevel
+import cats.effect._
+import cats.implicits._
+
+object Playground extends IOApp {
+  override def run(args: List[String]): IO[ExitCode] =
+    IO(println("hello world!"))
+      .as(ExitCode.Success)
+}
